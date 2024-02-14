d253b9ae685df7fa1497cba220a92b06acc6633f
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-02-14 08:42:28 +0530

    first commit

---
+++ src/main/java/com/example/springultimate/SpringUltimateApplication.java
@@ -0,0 +1,13 @@
+package com.example.springultimate;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringUltimateApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringUltimateApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springultimate/SpringUltimateApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springultimate;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringUltimateApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
