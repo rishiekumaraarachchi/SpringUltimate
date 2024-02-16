79159a28e2d66187c44e374e5985576a866d0bd0
rishiekumaraarachchi <rishieruween199611@gmail.com>
2024-02-16 09:57:19 +0530

    rest api commit

--- src/main/java/com/example/springultimate/FirstController.java
+++ src/main/java/com/example/springultimate/FirstController.java
@@ -5,7 +5,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 public class FirstController {
-    @GetMapping("/hello")
+   // @GetMapping("/hello")
     @ResponseStatus(HttpStatus.ACCEPTED)
     public String hello() {
         return "Hello, World!";
@@ -16,8 +16,21 @@ public class FirstController {
         return "Post request has been made!" + message;
     }
 
+
+
     @PostMapping("/post-order")
     public String post(@RequestBody Orders order) {
         return "Order has been placed!" + order.toString();
+    }
+
+
+    @GetMapping("/hello/{userName}")
+    public String pathVariable(@PathVariable String userName) {
+        return "Hello, " + userName + "!";
+    }
+
+    @GetMapping("/hello")
+    public String requestParam(@RequestParam("user-name") String userName, @RequestParam("age") int age) {
+           return "Hello, " + userName + "! You are " + age + " years old.";
     }
 }
---
+++ src/main/java/com/example/springultimate/Student.java
@@ -0,0 +1,64 @@
+package com.example.springultimate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student {
+    @Id
+    private Integer id;
+    private String firstname;
+    private String lastname;
+    private String email;
+    private int age;
+
+    public Student() {
+    }
+
+    public Student(String firstname, String lastname, String email, int age) {
+        this.firstname = firstname;
+        this.lastname = lastname;
+        this.email = email;
+        this.age = age;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getFirstname() {
+        return firstname;
+    }
+
+    public void setFirstname(String firstname) {
+        this.firstname = firstname;
+    }
+
+    public String getLastname() {
+        return lastname;
+    }
+
+    public void setLastname(String lastname) {
+        this.lastname = lastname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+}
